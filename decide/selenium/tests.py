046cc726f38e0ada206967ea857f51bdd3e0a2cc
Jeselirod <jeselirod@alum.us.es>
2020-01-08 18:30:03 +0100

    cambios para examen travis

--- decide/selenium/tests.py
+++ decide/selenium/tests.py
@@ -7,9 +7,9 @@ class TestSignup(unittest.TestCase):
         self.driver = webdriver.Firefox()
         
     def test_signup_fire(self):
-        self.driver.get("http://localhost:8000/admin/login/?next=/admin/")
-        self.driver.find_element_by_id('id_username').send_keys("jeselirod")
-        self.driver.find_element_by_id('id_password').send_keys("Se180uS748ii")
+        self.driver.get("http://localhost:5000/admin/login/?next=/admin/")
+        self.driver.find_element_by_id('id_username').send_keys("practica")
+        self.driver.find_element_by_id('id_password').send_keys("practica")
         self.driver.find_element_by_id('login-form').click()
         self.assertTrue(len(self.driver.find_elements_by_id('user-tools'))>0) 
     def tearDown(self):
